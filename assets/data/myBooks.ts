b9ff253863ab627248a0318fd5d3657000449f48
stelaura <52497181+stelaura@users.noreply.github.com>
2019-07-19 22:16:41 -0400

    Added build files

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,12 @@
+import { BookInfo } from '../../app/bookInfo';
+
+export const MYBOOKS: BookInfo[] = [
+    { bookName: "Mistborn", author: "Brandon Sanderson", genre: "Fantasy", 
+      publishYear: 2006, coverImg: "assets/images/mistborn.jpg" },
+    { bookName: "City of Pearl", author: "Karen Traviss", genre: "Science Fiction", 
+      publishYear: 2004, coverImg: "assets/images/cityOfPearl.jpg" },
+    { bookName: "The Handmaid's Tale", author: "Margaret Atwood", genre: "Classic", 
+      publishYear: 1985, coverImg: "assets/images/theHandmaidsTale.jpg" },
+    { bookName: "The Princess Bride", author: "William Goldman", genre: "Fantasy", 
+      publishYear: 1973, coverImg: "assets/images/thePrincessBride.jpg" }
+]
